6d9e61f599d65430eee116acae78097c99054855
Sougoumay <elidjaihamid@gmail.com>
2024-01-06 21:47:17 +0100

    configuration of user roles et restrict urls using memory user detail managers natives fonctionnalities of spring

---
+++ src/main/java/hamid/sougouma/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package hamid.sougouma.springboot.cruddemo.dao;
+
+import hamid.sougouma.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
---
+++ src/main/java/hamid/sougouma/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package hamid.sougouma.springboot.cruddemo.service;
+
+import hamid.sougouma.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
