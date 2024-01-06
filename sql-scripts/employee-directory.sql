47f561ea98d2054ed4dc03950db9ad1c93939b4d
Sougoumay <elidjaihamid@gmail.com>
2024-01-06 22:58:27 +0100

    configuration of user roles and restrict urls using jdbc

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -6,6 +6,8 @@ USE `employee_directory`;
 --
 
 DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -25,4 +27,61 @@ INSERT INTO `employee` VALUES
 	(3,'Avani','Gupta','avani@luv2code.com'),
 	(4,'Yuri','Petrov','yuri@luv2code.com'),
 	(5,'Juan','Vega','juan@luv2code.com');
+
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+    `username` varchar(50) NOT NULL,
+    `password` char(68) NOT NULL,
+    `active` tinyint NOT NULL,
+    CONSTRAINT user_primary_key_username PRIMARY KEY (`username`)
+);
+
+--
+-- Inserting data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.bcryptcalculator.com/
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users`
+VALUES
+    ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+    ('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+    ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `roles` (
+    `username` varchar(50) NOT NULL,
+    `role` varchar(50) NOT NULL,
+    CONSTRAINT `username_role_unique` UNIQUE KEY (`username`,`role`),
+    CONSTRAINT `fk_authorities_users_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `roles`
+VALUES
+    ('john','ROLE_EMPLOYEE'),
+    ('mary','ROLE_EMPLOYEE'),
+    ('mary','ROLE_MANAGER'),
+    ('susan','ROLE_EMPLOYEE'),
+    ('susan','ROLE_MANAGER'),
+    ('susan','ROLE_ADMIN');
+
+
+
+
 
--- src/main/java/hamid/sougouma/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/hamid/sougouma/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -9,32 +9,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC to avoid hardcoded users
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select username, password, active from users where username=?"
+        );
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+        // define query to retrieve the roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, role from roles where username=?"
+        );
 
-        return new InMemoryUserDetailsManager(john,mary,susan);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
